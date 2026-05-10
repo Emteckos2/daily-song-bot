5e6602d5607e5fc3e08fc1af965d17c8c9d613a6
emteckos2 <emteckos2@gmail.com>
2026-05-11 00:39:30 +0200

    rewrited to more robust logic

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"dailysongbot/internal/config"
 	"dailysongbot/internal/discord"
-	"dailysongbot/internal/errorlog"
 	"dailysongbot/internal/youtube"
 	"errors"
 	"fmt"
@@ -12,90 +11,76 @@ import (
 )
 
 func Scheduler(ctx context.Context) error {
+	// dont run if is cancelled on start
+	select {
+	case <-ctx.Done():
+		return nil
+	default:
+	}
+
+	// adds 1 day, it can work even with time shift
+	var (
+		cfg config.UserConfigDataStruct
+		err error
+	)
+
+	// synch time for ticker
+	select {
+	// fire after seconds are 00
+	case <-time.After(time.Until(time.Now().Truncate(time.Minute).Add(1 * time.Minute))):
+
+	// piecefully exit if called
+	case <-ctx.Done():
+		return nil
+	}
+
+	// one minute interval
+	ticker := time.NewTicker(1 * time.Minute)
+	defer ticker.Stop()
+
 	for {
-		// dont run if is cancelled on start
 		select {
-		case <-ctx.Done():
-			return nil
-		default:
-		}
-
-		// adds 1 day, it can work even with time shift
-		var (
-			ok  bool
-			ud  config.UserConfigDataStruct
-			err error
-		)
-		if ud, ok = config.UserConfig.Load().(config.UserConfigDataStruct); !ok {
-			return fmt.Errorf("user config load: nil data")
-		}
-
-		err = config.CheckConfigSettings(&ud)
-		if err != nil {
-			errorlog.Logger.Warn("config check setting: " + err.Error())
-			sleepWithContext(ctx, time.Minute)
-			if ctx.Err() != nil {
-				return nil
-			}
-			continue
-		}
-
-		last := ud.LastRunTime.UTC()
-		next := time.Date(last.Year(), last.Month(), last.Day(),
-			ud.PostHours, ud.PostMinutes, 0, 0, time.UTC).AddDate(0, 0, 1)
-
-		waitDuration := time.Until(next)
-		timer := time.NewTimer(waitDuration)
-
-		select {
-		// park gorutine until time come
-		case <-timer.C:
-			timer.Stop()
-			// load fresh infos
-			if ud, ok = config.UserConfig.Load().(config.UserConfigDataStruct); !ok {
-				return fmt.Errorf("user config load: nil data")
+		// checks if should be send
+		case <-ticker.C:
+			if cfg, err = config.Get(); err != nil {
+				return fmt.Errorf("user config load: %w", err)
 			}
 
-			err = config.CheckConfigSettings(&ud)
-			if err != nil {
-				errorlog.Logger.Warn("config check setting: " + err.Error())
+			if time.Now().Before(cfg.NextRunTime) {
 				continue
 			}
 
-			channelID := ud.ChannelID
-			playlistID := ud.PlaylistID
-
 			// send next song
-			err = discord.SendDailySong(ud.PlaylistNextSong,
-				channelID, playlistID)
+			err = discord.SendDailySong(cfg.PlaylistNextSong,
+				cfg.ChannelID, cfg.PlaylistID)
 
 			if err != nil {
 				if !errors.Is(err, youtube.ErrEndPlaylist) {
 					return fmt.Errorf("send daily song: %w", err)
 				}
 
-				ud.PlaylistNextSong = 1
+				cfg.PlaylistNextSong = 1
 
-				err = discord.SendDailySong(ud.PlaylistNextSong,
-					channelID, playlistID)
+				err = discord.SendDailySong(cfg.PlaylistNextSong,
+					cfg.ChannelID, cfg.PlaylistID)
 
 				if err != nil {
 					return fmt.Errorf("send daily song: %w", err)
 				}
 			}
 
-			ud.LastRunTime = time.Now().UTC()
-			ud.PlaylistNextSong++
+			now := time.Now()
+			cfg.NextRunTime = time.Date(now.Year(), now.Month(), now.Day(),
+				cfg.PostHours, cfg.PostMinutes, 0, 0, time.UTC).AddDate(0, 0, 1)
+			cfg.PlaylistNextSong++
 
-			config.UserConfig.Store(ud)
-			err = config.WriteConfig()
+			err = config.Save(&cfg)
 			if err != nil {
 				return fmt.Errorf("write config: %w", err)
 			}
 
-		// piecefully exit
+		// piecefully exit if called
 		case <-ctx.Done():
-			timer.Stop()
 			return nil
 		}
 	}
